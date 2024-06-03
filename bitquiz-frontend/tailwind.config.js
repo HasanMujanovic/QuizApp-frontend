665e35f05e846d4e7b6608be2f88b8295a52b559
HasanMujanovic <respiratori1234@gmail.com>
2024-06-03 23:17:20 +0200

    landing page V1.0

--- bitquiz-frontend/tailwind.config.js
+++ bitquiz-frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: "#10454F",
+        customGrayBlue: "#506266",
+        customBetween: "#818274",
+        customLessYellow: "#C6DF66",
+        customYellow: "#BDE038",
+      },
+    },
   },
   plugins: [],
 };
