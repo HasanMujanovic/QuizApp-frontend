23d919db93d5597d28ce9cf59eccb5b2562e3551
HasanMujanovic <respiratori1234@gmail.com>
2024-06-04 13:29:53 +0200

    sila apdejta

--- bitquiz-frontend/src/app/app.module.ts
+++ bitquiz-frontend/src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { QuizesComponent } from './components/quizes/quizes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CreateQuizComponent } from './components/create-quiz/create-quiz.component';
+
+const routes: Routes = [
+  { path: 'create-quiz', component: CreateQuizComponent },
+  { path: 'quizes', component: QuizesComponent },
+  { path: '', component: LandingPageComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    QuizesComponent,
+    LandingPageComponent,
+    CreateQuizComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
